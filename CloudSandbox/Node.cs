23b46f1aa65a5b0aea6d3eaa84c422ac8bbed971
Olav Salhus <olav.salhus@gmail.com>
2021-10-21 23:37:54 +0200

    TreeView reflection experiment

---
+++ CloudSandbox/Node.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CloudSandbox
+{
+    public class Node
+    {
+        public string id { get; set; }
+        public string text { get; set; }
+        public bool children { get; set; }
+    }
+}
